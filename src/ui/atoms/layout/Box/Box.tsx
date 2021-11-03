4cde494ff280103911958bdfacc9546b418251c7
Simone G <gaspari.simone@gmail.com>
2021-11-03 15:52:45 +0100

    fix box

--- src/ui/atoms/layout/Box/Box.tsx
+++ src/ui/atoms/layout/Box/Box.tsx
@@ -1,46 +1,36 @@
-import shouldForwardProp from '@styled-system/should-forward-prop';
-import styled from 'styled-components';
-import { borderRadius, padding } from "styled-system";
-import { width } from "styled-system";
-import { WidthProps } from "styled-system";
-import { BackgroundImageProps } from "styled-system";
+import shouldForwardProp from "@styled-system/should-forward-prop";
+import styled from "styled-components";
 import { LayoutProps, FlexboxProps, BorderProps, SpaceProps, MarginProps, ColorProps, BackgroundProps, PositionProps } from "styled-system";
-import { background, border, color, flexbox, layout, margin, position, space } from 'styled-system';
+import { background, border, color, flexbox, layout, margin, position, space } from "styled-system";
 
 
 /**
  * Box is the basic building block.
  * It abstracts a div on web and a View on mobile
  */
-export type BoxProps =
-    & SpaceProps
-    & WidthProps
-    & ColorProps
-    & BackgroundImageProps
-    & BackgroundProps
-    & LayoutProps
-    & FlexboxProps
-    & BorderProps
-    & FlexboxProps
-    & WidthProps
+export type BoxProps = LayoutProps &
+    FlexboxProps &
+    BorderProps &
+    SpaceProps &
+    MarginProps &
+    ColorProps &
+    BackgroundProps &
+    PositionProps;
 
 export const Box = styled.div.withConfig<BoxProps>({
-    shouldForwardProp: (prop: string) => ['onLayout'].indexOf(prop) >= 0 || shouldForwardProp(prop),
-})<BoxProps>(
-    borderRadius,
-    color,
-    space,
-    color,
-    space,
-    layout,
-    padding,
-    flexbox,
-    background,
-    border,
-    position,
-    width
+    shouldForwardProp: (prop: string) => ["onLayout"].indexOf(prop) >= 0 || shouldForwardProp(prop),
+}) <BoxProps>`
+    ${layout}
+    ${flexbox}
+    ${border}
+    ${space}
+    ${margin}
+    ${color}
+    ${background}
+    ${position}
+    box-sizing:border-box;
+`;
 
-);
 
 /**
 * A Box with flex and forced horizontal orientation.
