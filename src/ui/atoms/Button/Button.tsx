62e1f31c9c0d055f1a769180bc037a3a24d9ae87
Simone G <gaspari.simone@gmail.com>
2021-11-03 13:12:10 +0100

    fix button export

--- src/ui/atoms/Button/Button.tsx
+++ src/ui/atoms/Button/Button.tsx
@@ -5,7 +5,7 @@ import { ButtonProps, ThemedButton } from './Button.styled';
 /**
  * Button UI component for user interaction
  */
-const Button: React.FC<ButtonProps> = ({
+export const Button: React.FC<ButtonProps> = ({
     variant = "primary",
     label,
     ...props
@@ -15,7 +15,7 @@ const Button: React.FC<ButtonProps> = ({
     const baseTheme = useMemo(() => {
         const { variants, ...restOfTheme } = buttonTheme;
         return restOfTheme;
-    }, [theme])
+    }, [buttonTheme])
     console.log("base theme is", baseTheme)
     return (
         <ThemedButton {...baseTheme} variant={variant} {...props} >
