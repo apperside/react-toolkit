307e2a0ae7ddfa186f0c22c326134e656a7ce803
Simone G <gaspari.simone@gmail.com>
2021-12-03 18:19:59 +0100

    add export useTask

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from "./useOperation/reducers"
 export { useOperation } from "./useOperation"
+export { useTask } from "./useTask"
 export { useAppQuery } from "./useAppQuery"
 export { useAppMutation } from "./useAppMutation"
 export { useScreen } from "./useScreen"
