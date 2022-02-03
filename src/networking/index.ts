26dbba5359b4086dc92d6c73b67d4682311be258
Simone G <gaspari.simone@gmail.com>
2022-02-03 12:52:22 +0100

    improve api typings

--- src/networking/index.ts
+++ src/networking/index.ts
@@ -1,8 +1,39 @@
 export * from "./restManager"
 export * from "./httpManager"
 
-export interface MainApi {
-  // "auth/login": { dataType: { result: boolean, token: string }, payloadType: { email: string, password: string } }
+type BasePath<T extends string | unknown = unknown> = `${T extends string ? T : ""}${T extends string ? "/" : ""}`
+type AllRoutes<S extends string, Prefix extends string | unknown = unknown> =
+  `${BasePath<Prefix>}${S}` |
+  `${BasePath<Prefix>}${S}/:id`
+  | `${BasePath<Prefix>}${S}/bulk`
+
+export interface User {
+
+}
+export interface RouteModelMapping {
+  user: User
+}
+export type ModelMapping<T extends keyof RouteModelMapping, BasePath extends string | unknown = unknown> = {
+  [key in AllRoutes<T, BasePath>]: RouteModelMapping[T]
+}
+
+
+export interface ModelsDef { }
+
+
+export type CrudRoutes = {
+  // eslint-disable-next-line max-len
+  [key in keyof ModelsDef]:
+  key extends `${string}/bulk` ?
+  { dataType: { data: never }, payloadType: { data: ModelsDef[key][] } } :
+  key extends `${string}/:id` ?
+  { dataType: { data: ModelsDef[key] }, payloadType: ModelsDef[key] } :
+  key extends `${string}` ?
+  { dataType: { data: ModelsDef[key][] }, payloadType: never } :
+  unknown
+}
+
+export interface MainApi extends CrudRoutes {
 }
 
 // interface IApiConfig { [key: string]: { [key: string]: { dataType: any, payloadType?: any } } }
