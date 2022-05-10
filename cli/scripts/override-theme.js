450a31e3f85aa21ced99701a36dcf9db85b0ee96
Simone G <gaspari.simone@gmail.com>
2022-05-10 09:39:52 +0200

    fix cli script

--- cli/scripts/override-theme.js
+++ cli/scripts/override-theme.js
@@ -1,32 +1,37 @@
-import * as fs from "fs-extra"
-import * as path from "path"
+const fs = require("fs-extra");
+const path = require("path");
 
-import readline from "readline";
+const readline = require("readline");
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 rl.question("In which path you want to add the theme files?", (response) => {
-	const args = process.argv.slice(2);
-	console.log("args", args);
-	const dest = path.join(process.cwd(), response);
-	fs.ensureDirSync(dest);
-	fs.copySync(path.join(__dirname, "../templates/ui"), dest, { errorOnExist: true });
+  const args = process.argv.slice(2);
+  console.log("args", args);
+  const dest = path.join(__dirname, "../../../..", response);
+  console.log("dest", dest);
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true });
+  }
+  fs.copySync(path.join(__dirname, "../templates/ui"), dest, {
+    errorOnExist: true,
+  });
+  const renameAllFiles = (rootDir) => {
+    fs.readdirSync(rootDir).forEach((file) => {
+      const fullPath = path.join(rootDir, file);
+      if (fs.lstatSync(fullPath).isDirectory()) {
+        console.log(fullPath);
+        renameAllFiles(fullPath);
+      } else {
+        fs.renameSync(fullPath, fullPath.replace(".hbs", ""));
+        console.log(fullPath);
+      }
+    });
+  };
 
-	const renameAllFiles = (rootDir) => {
-		fs.readdirSync(rootDir).forEach(file => {
-			const fullPath = path.join(rootDir, file);
-			if (fs.lstatSync(fullPath).isDirectory()) {
-				console.log(fullPath);
-				renameAllFiles(fullPath);
-			} else {
-				fs.renameSync(fullPath, fullPath.replace(".hbs", ""));
-				console.log(fullPath);
-			}
-		});
-	}
+  renameAllFiles(dest);
+});
 
-	renameAllFiles(path.join(process.cwd(), response));
-})
