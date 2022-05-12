d2962d845fbe867a25a04908e2885c0a55a1eda8
Simone G <gaspari.simone@gmail.com>
2022-05-12 12:11:25 +0200

    add underline prop to Text

--- src/ui/atoms/Text/Text.styled.tsx
+++ src/ui/atoms/Text/Text.styled.tsx
@@ -1,56 +1,76 @@
 import { CursorProps, PropsWithVariant } from "../../theme";
-import styled from 'styled-components';
+import styled from "styled-components";
 import {
-    BackgroundImageProps, border, BorderProps, borderRadius, color,
-    ColorProps, flexbox, FlexboxProps, FontFamilyProps, fontSize, layout, LayoutProps, padding,
-    position, space, SpaceProps, textStyle, TextStyleProps as SSTextStyleProps, typography,
-    TypographyProps as SSTypographyProps, variant, WidthProps
-} from 'styled-system';
+  BackgroundImageProps,
+  border,
+  BorderProps,
+  borderRadius,
+  color,
+  ColorProps,
+  flexbox,
+  FlexboxProps,
+  FontFamilyProps,
+  fontSize,
+  layout,
+  LayoutProps,
+  padding,
+  position,
+  space,
+  SpaceProps,
+  textStyle,
+  TextStyleProps as SSTextStyleProps,
+  typography,
+  TypographyProps as SSTypographyProps,
+  variant,
+  WidthProps,
+} from "styled-system";
 import { TextVariants } from "./Text.theme";
 
-export type TextStyleProps =
-    & SSTypographyProps
-    & SpaceProps
-    & WidthProps
-    & ColorProps
-    & BackgroundImageProps
-    & SSTextStyleProps
-    & LayoutProps
-    & FlexboxProps
-    & FontFamilyProps
-    & BorderProps
-    & CursorProps
+export type TextStyleProps = SSTypographyProps &
+  SpaceProps &
+  WidthProps &
+  ColorProps &
+  BackgroundImageProps &
+  SSTextStyleProps &
+  LayoutProps &
+  FlexboxProps &
+  FontFamilyProps &
+  BorderProps &
+  CursorProps;
 
-export interface TextProps extends TextStyleProps, PropsWithVariant<keyof TextVariants> {
-    // children: React.ReactNode;
-    // label?: string
-    // onClick?: () => void
+export interface TextProps
+  extends TextStyleProps,
+    PropsWithVariant<keyof TextVariants> {
+  // children: React.ReactNode;
+  // label?: string
+  // onClick?: () => void
+  underlined?: boolean;
 }
 
 export const ThemedText = styled.span<TextProps>(
-    {
-        // appearance: 'text',
-        border: 0,
-        outline: 0,
-    },
-    borderRadius,
-    // textSize,
-    textStyle,
-    color,
-    space,
-    fontSize,
-    color,
-    space,
-    layout,
-    padding,
-    flexbox,
-    border,
-    typography,
-    position,
-    props => {
-        return variant({
-            variants: props.theme.text.variants as any
-        })
-    },
-
+  {
+    // appearance: 'text',
+    border: 0,
+    outline: 0,
+  },
+  borderRadius,
+  // textSize,
+  textStyle,
+  color,
+  space,
+  fontSize,
+  color,
+  space,
+  layout,
+  padding,
+  flexbox,
+  border,
+  typography,
+  position,
+  (props) => {
+    return variant({
+      variants: props.theme.text.variants as any,
+    });
+  }
 );
+
--- src/ui/atoms/Text/Text.tsx
+++ src/ui/atoms/Text/Text.tsx
@@ -1,43 +1,59 @@
-import React from 'react';
+import React from "react";
 import { DefaultTheme, StyledComponent, useTheme } from "styled-components";
 import { PropsOf } from "../../../typeUtils";
-import { TextProps, ThemedText } from './Text.styled';
+import { TextProps, ThemedText } from "./Text.styled";
 import { useMemo } from "react";
-import deepmerge from "deepmerge";
 
 const BaseTextComponent: React.FC<PropsOf<typeof ThemedText> & TextProps> = ({
-    variant = "regular",
-    ...props
+  variant = "regular",
+  underlined,
+  style,
+  ...props
 }) => {
+  const theme = useTheme();
+  const textTheme = theme.text;
 
-    const theme = useTheme();
-    const textTheme = theme.text;
+  const baseTheme = useMemo(() => {
+    const { variants, ...restOfTheme } = textTheme;
+    return restOfTheme;
+  }, [textTheme]);
 
-    const baseTheme = useMemo(() => {
-        const { variants, ...restOfTheme } = textTheme;
-        return restOfTheme;
-    }, [textTheme])
+  // const variantTheme = useMemo(() => {
+  //     const { variants, ...restOfTheme } = textTheme;
+  //     return variants[variant]!;
+  // }, [textTheme, variant])
 
-    const variantTheme = useMemo(() => {
-        const { variants, ...restOfTheme } = textTheme;
-        return variants[variant]!;
-    }, [textTheme, variant])
+  // const textThemeProps = useMemo(() => {
+  //     const result = deepmerge(baseTheme, variantTheme, { clone: true });
+  //     console.log("button theme deep merge", result)
+  //     return result;
+  // }, [baseTheme, variantTheme])
 
-    // const textThemeProps = useMemo(() => {
-    //     const result = deepmerge(baseTheme, variantTheme, { clone: true });
-    //     console.log("button theme deep merge", result)
-    //     return result;
-    // }, [baseTheme, variantTheme])
+  const finalStyle = useMemo(() => {
+    if (underlined) {
+      return { textDecoration: "underline", ...style };
+    }
+    return style;
+  }, [style, underlined]);
 
+  return (
+    <ThemedText {...baseTheme} variant={variant} style={finalStyle} {...props}>
+      {props.children}
+    </ThemedText>
+  );
+};
 
-    return (
-        <ThemedText {...baseTheme} variant={variant} {...props} >
-            {props.children}
-        </ThemedText >
-    )
-}
+export const Text = BaseTextComponent as StyledComponent<
+  "span",
+  DefaultTheme,
+  PropsOf<typeof ThemedText> & TextProps,
+  never
+>;
 
-export const Text = BaseTextComponent as StyledComponent<"span", DefaultTheme, PropsOf<typeof ThemedText> & TextProps, never>
+export default Text as StyledComponent<
+  "span",
+  DefaultTheme,
+  PropsOf<typeof ThemedText> & TextProps,
+  never
+>;
 
-
-export default Text as StyledComponent<"span", DefaultTheme, PropsOf<typeof ThemedText> & TextProps, never>
